ddca568d15c3d280f33197ca7bb54008b0849698
Hamed Abd <hamed59556@gmail.com>
2025-02-26 16:07:28 +0200

    Second Task

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,40 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/about', function () {
+    $name = 'Hamed';
+    $departments = [
+        '01' => 'Tichnical',
+        '02' => 'Financial',
+        '03' => 'Sales',
+    ];
+    // return view('about')->with('name', $name);
+    // return view('about', ['name' => $name]);
+    return view('about', compact('name', 'departments'));
+});
+
+Route::post('/about', function () {
+    $name = $_POST['name'];
+    $departments = [
+        '01' => 'Tichnical',
+        '02' => 'Financial',
+        '03' => 'Sales',
+    ];
+    return view('about', compact('name', 'departments'));
+});
+
+Route::get('tasks', function () {
+    return view('tasks');
+});
+Route::post('create', function () {
+    $name = $_POST['name'];
+
+    DB::table('tasks')->insert(['name' => $name]);
+    return view('tasks');
 });
